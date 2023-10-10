614c76418e6912bfe23747021c6edf21a829ffed
ts <tejfsingh@gmail.com>
2023-10-10 21:00:21 +0100

    support for triton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,77 +4,31 @@ import Editor, { Monaco } from "@monaco-editor/react";
 import React, { useRef, useState, useEffect } from 'react';
 
 const defaultCuda = `// Here's some CUDA to get you started!
-#include <iostream>
-#include <cstdlib>
+// ... (your CUDA code)
+`;
 
-__global__ void addArrays(int *a, int *b, int *c, int size) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < size) {
-        c[idx] = a[idx] + b[idx];
-    }
-}
-
-int main() {
-    // Defines the size of the arrays
-    int size = 5;
-
-    // Allocates memory on host
-    int *h_a, *h_b, *h_c;
-    h_a = (int *)malloc(size * sizeof(int));
-    h_b = (int *)malloc(size * sizeof(int));
-    h_c = (int *)malloc(size * sizeof(int));
-
-    // Initializes arrays
-    for (int i = 0; i < size; i++) {
-        h_a[i] = i;
-        h_b[i] = i * 2;
-    }
-
-    // Allocates memory on the device
-    int *d_a, *d_b, *d_c;
-    cudaMalloc((void **)&d_a, size * sizeof(int));
-    cudaMalloc((void **)&d_b, size * sizeof(int));
-    cudaMalloc((void **)&d_c, size * sizeof(int));
-
-    // Copies data from host to device
-    cudaMemcpy(d_a, h_a, size * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, size * sizeof(int), cudaMemcpyHostToDevice);
-
-    // Defines the grid and block dimensions
-    int blockSize = 256;
-    int gridSize = (size + blockSize - 1) / blockSize;
-
-    // Launches the CUDA kernel
-    addArrays<<<gridSize, blockSize>>>(d_a, d_b, d_c, size);
-
-    // Copies the result from device to host
-    cudaMemcpy(h_c, d_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-
-    // Prints the result
-    for (int i = 0; i < size; i++) {
-        std::cout << h_c[i] << " ";
-    }
-    std::cout << std::endl;
-
-    // Free device and host memory
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_c);
-    free(h_a);
-    free(h_b);
-    free(h_c);
-
-    return 0;
-}
+const defaultTriton = `# Here's some Python code for Triton
+# ... (your Python code)
 `;
 
 const App = () => {
+  const [selectedLanguage, setselectedLanguage] = useState<string>(() => {
+    const storedLanguage = localStorage.getItem('selectedLanguage');
+    return storedLanguage || "cuda";
+  });
+
   const [cudaCode, setCudaCode] = useState<string | undefined>(() => {
     const storedCudaCode = localStorage.getItem('cudaCode');
     return storedCudaCode || defaultCuda;
   });
+  const [tritonCode, setTritonCode] = useState<string | undefined>(() => {
+    const storedTritonCode = localStorage.getItem('tritonCode');
+    return storedTritonCode || defaultTriton;
+  });
 
-  const [result, setResult] = useState<string>("[0, 3, 6, 9, 12]");
+  const [cudaResult, setCudaResult] = useState<string>(">> [0, 3, 6, 9, 12]");
+  const [tritonResult, setTritonResult] = useState<string>(">> [0, 3, 6, 9, 12]");
+
   const editorRef = useRef<any>(null);
 
   function handleEditorDidMount(editor: any, monaco: any) {
@@ -82,39 +36,87 @@ const App = () => {
   }
 
   useEffect(() => {
-    localStorage.setItem('cudaCode', cudaCode || '');
-  }, [cudaCode]);
+    if (selectedLanguage === "cuda") {
+      localStorage.setItem('cudaCode', cudaCode || '');
+    } else if (selectedLanguage === "triton") {
+      localStorage.setItem('tritonCode', tritonCode || '');
+    }
+    localStorage.setItem('selectedLanguage', selectedLanguage);
+  }, [cudaCode, tritonCode, selectedLanguage]);
 
-  function sendKernel() {
+  function sendCuda() {
     if (editorRef.current == null) {
       return;
     }
 
     let executionResult = editorRef.current.getValue();
-    
-    setResult(executionResult);
+    setCudaResult(executionResult);
   }
+
+  function sendTriton() {
+    if (editorRef.current == null) {
+      return;
+    }
+
+    let executionResult = editorRef.current.getValue();
+    setTritonResult(executionResult);
+  }
+
+  const result = selectedLanguage === "cuda" ? cudaResult : tritonResult;
+  const sendFunction = selectedLanguage === "cuda" ? sendCuda : sendTriton;
 
   return (
     <div className="flex flex-row">
-      <div className="w-32px">
-        <Editor
-          height="100vh"
-          width="70vw"
-          defaultLanguage="cpp"
-          value={cudaCode} 
-          theme="vs-dark"
-          onMount={handleEditorDidMount}
-          onChange={(newCudaCode : string | undefined) => setCudaCode(newCudaCode)} 
-        ></Editor>
+      <div>
+      {selectedLanguage === "cuda" ? (
+          <Editor
+            height="100vh"
+            width="70vw"
+            defaultLanguage="cpp"
+            value={cudaCode}
+            theme="vs-dark"
+            onMount={handleEditorDidMount}
+            onChange={(newCudaCode: string | undefined) =>
+              setCudaCode(newCudaCode)
+            }
+          ></Editor>
+        ) : (
+          <Editor
+            height="100vh"
+            width="70vw"
+            defaultLanguage="python"
+            value={tritonCode}
+            theme="vs-dark"
+            onMount={handleEditorDidMount}
+            onChange={(newTritonCode: string | undefined) =>
+              setTritonCode(newTritonCode)
+            }
+          ></Editor>
+        )}
       </div>
-      
       <div className="flex flex-col w-screen h-screen">
-        <button className="bg-blue-500 text-white text-lg py-1 px-4" onClick={sendKernel}>
-          Execute Kernel
-        </button> 
-        <pre className="text-sm text-zinc-300 pt-1 float-right font-mono overflow-y-auto" style={{ whiteSpace: "pre-wrap" }}>&gt;&gt; {result}</pre>
-      </div>
+        <div className="flex flex-row">
+          <select
+            className="bg-green-500 text-white text-lg py-1 px-4 w-1/2 rounded"
+            onChange={(e) => setselectedLanguage(e.target.value)}
+            value={selectedLanguage}>
+            <option value="cuda">CUDA</option>
+            <option value="triton">Triton</option>
+          </select>
+          <button
+            className="bg-blue-500 text-left text-white text-lg py-1 px-4 w-1/2 rounded"
+            onClick={sendFunction}
+          >
+            Execute Kernel
+          </button>
+        </div>
+        <pre
+          className="text-sm text-zinc-300 pt-1 float-right font-mono overflow-y-auto"
+          style={{ whiteSpace: "pre-wrap" }}
+        >
+          {result}
+        </pre>
+      </div>   
     </div>
   );
 }
