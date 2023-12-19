cbabd38083f19f7dbe8d0aea5f20db6e9df42fb6
Tej Singh <tejfsingh@gmail.com>
2023-12-19 19:17:58 +0530

    kernel execution working

--- server/kernel_execute.py
+++ server/kernel_execute.py
@@ -1,36 +1,46 @@
 import modal
 
-triton_image = modal.Image.debian_slim().pip_install("triton")
+triton_image = modal.Image.debian_slim().pip_install("triton", "torch")
 
-stub = modal.Stub("given")
+with triton_image.imports():
+    import torch
+    import triton
+    import triton.language as tl
 
-@stub.function(image=triton_image)
-def execute_kernel(function):
-    exec(function)
+stub = modal.Stub("cu-online")
 
-function = """
-import triton
-import triton.language as tlp
+@stub.function(gpu="T4", image=triton_image)
+def execute_kernel():
+    @triton.jit
+    def add_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+        pid = tl.program_id(axis=0)
+        block_start = pid * BLOCK_SIZE
+        offsets = block_start + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+        x = tl.load(x_ptr + offsets, mask=mask)
+        y = tl.load(y_ptr + offsets, mask=mask)
+        output = x + y
+        tl.store(output_ptr + offsets, output, mask=mask)
 
-@triton.jit
-def add(x, y):
-  # Define output tensor
-  output = tlp.zeros((x.shape[0],), dtype=x.dtype)
+    def add(x: torch.Tensor, y: torch.Tensor):
+        output = torch.empty_like(x)
+        assert x.is_cuda and y.is_cuda and output.is_cuda
+        n_elements = output.numel()
+        grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
+        add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+        return output
 
-  # Loop through elements in parallel
-  with tlp.for_() as i:
-    output[i] = x[i] + y[i]
-
-  return output
-
-# Usage example
-a = triton.arange(10)
-b = triton.ones(10)
-c = add(a, b)
-print(c) 
-"""
+    torch.manual_seed(0)
+    size = 98432
+    x = torch.rand(size, device='cuda')
+    y = torch.rand(size, device='cuda')
+    output_torch = x + y
+    output_triton = add(x, y)
+    print(output_torch)
+    print(output_triton)
+    print(f'The maximum difference between torch and triton is {torch.max(torch.abs(output_torch - output_triton))}')
 
 @stub.local_entrypoint()
 def main():
-    execute_kernel.remote(function)
+    execute_kernel.remote()
 
