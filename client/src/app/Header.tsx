d57e24aefce70761f6bed219ed4b98462b326faf
Tej Singh <tejfsingh@gmail.com>
2023-12-21 20:16:52 +0530

    refactored header

--- client/src/app/Header.tsx
+++ client/src/app/Header.tsx
@@ -1,1 +1,66 @@
+import Image from 'next/image';
+import { signIn, signOut } from "next-auth/react";
+import githubLogo from ".//../../public/github-logo.png"
 
+interface HeaderProps {
+    session: any;
+    selectedLanguage: string;
+    setSelectedLanguage: React.Dispatch<React.SetStateAction<string>>;
+    sendFunction: () => void;
+}
+
+const Header: React.FC<HeaderProps> = ({
+    session,
+    selectedLanguage,
+    setSelectedLanguage,
+    sendFunction,
+}) => {
+
+    return (
+        <div className="flex flex-row">
+            <div className="w-full bg-slate-900 text-white text-lg py-1 px-4">
+                Accelerated Computing, Online
+            </div>
+            <div className="bg-slate-900">
+                <a href="https://github.com/tfsingh/aconline">
+                    <Image src={githubLogo} height={50} width={50} alt="github" />
+                </a>
+            </div>
+            {!session?.user?.name ? (
+                <button
+                    onClick={() => signIn()}
+                    type="button"
+                    className="border-2 border-emerald-500 btn btn-primary bg-slate-900 text-white py-1 px-4 w-5/12 text-base rounded"
+                >
+                    Sign In
+                </button>
+            ) : (
+                <div className="flex flex-row w-5/12">
+                    <select
+                        className="border-2 border-blue-500 text-center text-white py-1 px-4 w-5/12 text-base bg-slate-900 rounded-l"
+                        onChange={(e) => setSelectedLanguage(e.target.value)}
+                        value={selectedLanguage}
+                    >
+                        <option value="triton">Triton/Numba</option>
+                        <option value="cuda">CUDA</option>
+                    </select>
+                    <button
+                        className="border-2 border-emerald-500 bg-slate-900 text-white w-1/3 py-1 px-4 text-base"
+                        onClick={sendFunction}
+                    >
+                        Run Kernel
+                    </button>
+                    <button
+                        onClick={() => signOut()}
+                        type="button"
+                        className="border-2 border-red-500 btn btn-primary text-white w-1/3 py-1 px-4 text-base bg-slate-900 rounded-r"
+                    >
+                        Sign Out
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Header;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Editor from "@monaco-editor/react";
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { defaultCuda, defaultTriton, defaultLanguage, defaultCudaResult, defaultTritonResult } from "./constants";
-import { useSession, signIn, signOut } from "next-auth/react";
-import Image from "next/image";
-import githubLogo from ".//../../public/github-logo.png"
+import { useSession } from "next-auth/react";
+
 import useLocalStorageState from "./LocalStorageState";
+import Header from "./Header";
 
 export default function App() {
   const { data: session } = useSession();
@@ -72,61 +72,23 @@ export default function App() {
     }
   }
 
+
   const result = selectedLanguage === "cuda" ? cudaResult : tritonResult;
   const sendFunction = selectedLanguage === "cuda" ? sendCuda : sendTriton;
   return (
     <div className="flex flex-col">
-      <div className="flex flex-row">
-
-        <div className="w-full bg-slate-900 text-white text-lg py-1 px-4">
-          Accelerated Computing, Online
-        </div>
-        <div className="bg-slate-900">
-          <a href="https://github.com/tfsingh/aconline"><Image src={githubLogo} height={50} width={50} alt="github" /></a>
-        </div>
-        {!session?.user?.name ? (
-          <button
-            onClick={() => signIn()}
-            type="button"
-            className="border-2 border-emerald-500 btn btn-primary bg-slate-900 text-white py-1 px-4 w-5/12 text-base rounded"
-          >
-            Sign In
-          </button>
-        ) : (
-          <div className="flex flex-row w-5/12">
-
-            <select
-              className="border-2 border-blue-500 text-center text-white py-1 px-4 w-5/12 text-base bg-slate-900 rounded-l"
-              onChange={(e) => setSelectedLanguage(e.target.value)}
-              value={selectedLanguage}
-            >
-              <option value="triton">Triton/Numba</option>
-              <option value="cuda">CUDA</option>
-            </select>
-            <button
-              className="border-2 border-emerald-500 bg-slate-900 text-white w-1/3 py-1 px-4 text-base"
-              onClick={sendFunction}
-            >
-              Run Kernel
-            </button>
-            <button
-              onClick={() => signOut()}
-              type="button"
-              className="border-2 border-red-500 btn btn-primary text-white w-1/3 py-1 px-4 text-base bg-slate-900 rounded-r"
-            >
-              Sign Out
-            </button>
-
-          </div>
-        )}
-
-      </div>
+      <Header
+        session={session}
+        selectedLanguage={selectedLanguage}
+        setSelectedLanguage={setSelectedLanguage}
+        sendFunction={sendFunction}
+      />
       <div className="flex flex-row h-screen">
         <div>
           {selectedLanguage === "cuda" ? (
             <Editor
               height="100vh"
-              width="71vw"
+              width="70vw"
               language="cpp"
               value={cudaCode}
               theme="vs-dark"
@@ -138,7 +100,7 @@ export default function App() {
           ) : (
             <Editor
               height="100vh"
-              width="71vw"
+              width="70vw"
               language="python"
               value={tritonCode}
               theme="vs-dark"
